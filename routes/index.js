908a0e78cb0aa036129a0ee0a49bfe305dcd050a
MinamiHanashiro <minamihanashiro@keio.jp>
2018-02-06 02:47:40 +0900

    index.ejsとindex.jsいじった

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-var express = require('express');
+var exprvar express = require('express');
 var router = express.Router();
 
 /* GET home page. */
@@ -6,7 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
 router.post('/', function(req, res, next) {
   var title = req.body.title;
   console.log(title);
 });
+
+module.exports = router;
