74aa315a4abc695f30e9ff0eaf25190c02c9a2fa
MinamiHanashiro <minamihanashiro@keio.jp>
2018-02-05 20:08:33 +0900

    モーメントjs導入したど

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment'); // 追加
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,5 +9,9 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
   var title = req.body.title;
+  var createdAt = moment().format('YYYY-MM-DD HH:mm:ss'); // 追加
   console.log(title);
+  console.log(createdAt); // 追加
 });
+
+module.exports = router;
