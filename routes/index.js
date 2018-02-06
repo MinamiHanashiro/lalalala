00d52f25509b77dcb6b93ad4ecc1df4a5f20395b
MinamiHanashiro <minamihanashiro@keio.jp>
2018-02-06 10:49:56 +0900

    更新

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
-var exprvar express = require('express');
+var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
 
 router.post('/', function(req, res, next) {
   var title = req.body.title;
