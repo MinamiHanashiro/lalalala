6a04b829e74f724f1c4c6b9358e9962a9bd62085
im-neko <lukepuyuki0430@gmail.com>
2018-02-08 23:31:36 +0900

    add try catch

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,14 @@ var connection = require('../mysqlConnection'); // 追加
 router.get('/', function(req, res, next) {
   var query = 'SELECT *, DATE_FORMAT(created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM boards';
   connection.query(query, function(err, rows) {
-    res.render('index', {
-      title: 'はじめてのNode.js',
-      boardList: rows
-    });
+    try{
+      res.render('index', {
+        title: 'はじめてのNode.js',
+        boardList: rows
+      });
+    } catch(e){
+      console.log(e);
+    }
   });
 });
 
