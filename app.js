c5f30148804b341ee5022ba0724d84e369fec130
MinamiHanashiro <minamihanashiro@keio.jp>
2018-02-12 09:32:14 +0900

    名前の紐付けできるようになった

--- app.js
+++ app.js
@@ -44,6 +44,10 @@ app.use('/logout', logout);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
+  if( err ) {
+    console.log(err);
+    return;
+  } //付け足してみた
   err.status = 404;
   next(err);
 });
--- routes/boards.js
+++ routes/boards.js
@@ -27,10 +27,14 @@ router.get('/:board_id', function(req, res, next) {
 router.post('/:board_id', function(req, res, next) {
   var message = req.body.message;
   var boardId = req.params.board_id;
-var userId = req.session.user_id? req.session.user_id: 0; // 追加
-var createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
-var query = 'INSERT INTO messages (message, board_id, user_id, created_at) VALUES ("' + message + '", ' + '"' + boardId + '", ' + '"' + userId + '", ' + '"' + createdAt + '")'; // 変更
-connection.query(query, function(err, rows) {
+  var userId = req.session.user_id? req.session.user_id: 0; // 追加 var createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
+  var createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
+  var query = 'INSERT INTO messages (message, board_id, user_id, created_at) VALUES ("' + message + '", ' + '"' + boardId + '", ' + '"' + userId + '", ' + '"' + createdAt + '")'; // 変更 connection.query(query, function(err, rows) {
+    connection.query(query, function(err, rows) {
+      if( err ) {
+        console.log(err);
+        return;
+      }
     res.redirect('/boards/' + boardId);
   });
 });
